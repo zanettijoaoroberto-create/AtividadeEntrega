84db8f3c7ad9e2a72dbdf4cae321473388c65f16
zanettijoaoroberto-create <zanettijoaoroberto@gmail.com>
2026-05-06 17:10:48 -0300

    javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 // ATIVIDADE 01
 const ligar = () => {
+    let lampada = document.getElementById("lampada");
 
+    if(lampada.src.includes("apagada.jpg")){
+        lampada.src = "img/acesa.jpg";
+    } else {
+        lampada.src = "img/apagada.jpg";
+    }
 };
 // -----------------------------------------
 
 // ATIVIDADE 02
 const gerarImagem = () => {
+    let qtdImagem = Number(document.getElementById("inQtdImg").value);
+    let canvas = document.getElementById("canvas");
+    canvas.innerHTML = "";
 
+    if (qtdImagem <= 0) {
+        alert("O campo quantidade não pode estar nulo ou negativo!");
+    } else {
+        for (let i = 0; i < qtdImagem; i++) {
+            canvas.innerHTML += `<img id="logo" src="img/logo.png"
+            alt="Logotipo SENAI" class="imagens" />`;
+        }
+ }
 };
 // -----------------------------------------
 
